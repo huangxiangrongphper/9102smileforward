f3555caf36031e886467026819597a3f64df0a9b
Flourishing <huangxiangrong827@163.com>
2019-03-02 11:30:25 +0800

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = '黄向荣';
         $user->email = 'huangxiangrong827@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
