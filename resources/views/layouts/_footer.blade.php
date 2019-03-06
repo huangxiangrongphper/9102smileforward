3cc5e2df016963ade418ed75efaf88ead1cf0f04
Flourishing <huangxiangrong827@163.com>
2019-03-07 00:30:51 +0800

    主页修改

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="footer">
     <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
-    <a href="https://learnku.com/laravel/courses" target=_blank>
-        刻意练习，每日精进
+    <a href="https://laravel.com" target=_blank>
+        由 Laravel 强力驱动.
     </a>
 
     <div class="float-right">
-        <a href="{{ route('about') }}" >关于</a>
+        <a href="{{ route('about') }}" >关于作者</a>
     </div>
 </footer>
